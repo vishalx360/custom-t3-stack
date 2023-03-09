94e174c205ac1a5b6767802c4d12b056a5d3ab15
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-03-09 19:17:08 +0530

    switch to next experimental appDir

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-module.exports = config;
+}
